48199bc2e92a489323a837eef589e1ee5316cd3f
HEHEHEHEHEHAHHAAH <133034453+HEHEHEHEHEHAHHAAH@users.noreply.github.com>
2025-05-08 06:33:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('changeMessageButton').addEventListener('click', function() {
+    const greetingElement = document.getElementById('greeting');
+    const thankYouMessageElement = document.getElementById('thank-you-message');
+
+    greetingElement.textContent = "SEKALI LAGI";
+    thankYouMessageElement.textContent = "Lo tuh emang terbaik! SEMANGAT BERJUANG DI KEHIDUPAN BARU INGET GW SELALU ADA KALO LU BUTUH APA APA ANJAY";
+});
